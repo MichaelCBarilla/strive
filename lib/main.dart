712091e6f5830f34b08e12801c9cb0e83fecf68d
Michael Barilla <michael@eeproto.com>
2024-02-24 12:51:41 -0500

    remove import

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:strive/firebase_options.dart';
-import 'package:strive/providers/exercises.dart';
 
 import 'package:strive/screens/auth_screen.dart';
 import 'package:strive/screens/loading_screen.dart';
